935a3eccbfdb3c694fb77dcf569275efa5b10e34
Théo Giovedi <tmgiovedi@gmail.com>
2023-04-13 13:30:21 -0300

    k-means for palettes

--- qual-a-cor/scripts/palettes.js
+++ qual-a-cor/scripts/palettes.js
@@ -137,84 +137,75 @@ function camDraw() {
 function getPalette() {
     let camPixels = camContext.getImageData(0, 0, w, h);
 
-    // Median Cut Algorithm to determine the most dominant colors in an image
+    // K-means algorithm for extracting a palette of 4 colors
 
-    // Part 1: Determine the color channel (R, G or B) with the largest range
+    let k = 4;
+    let sum = new Array(k);
+    let clusterLength = new Array(k);
+    let centroids = new Array(k);
 
-    let rgb = [];
-    let redMax = 0, greenMax = 0, blueMax = 0;
-    let redMin = 255, greenMin = 255, blueMin = 255;
+    // Forgy's Initialization: Randomly choosing k observations from data set
+    for (let i = 0; i < k; i++) {
+        let random = Math.floor(Math.random() * camPixels.data.length);
+        centroids[i] = [ 
+            camPixels.data[random - (random % 4)], // get the nearest multiple of 4 less than random
+            camPixels.data[random - (random % 4) + 1], 
+            camPixels.data[random - (random % 4) + 2]
+        ];
+        sum[i] = [ 0, 0, 0 ];
+        clusterLength[i] = 0;
+    }
+    
     for (let i = 0; i < camPixels.data.length; i += 4) {
-        rgb.push([
+
+        // Assign the observation to the closest cluster
+
+        let rgb = [
             camPixels.data[i],
             camPixels.data[i + 1],
             camPixels.data[i + 2]
-        ]);
+        ];
 
-        redMax = Math.max(rgb[rgb.length - 1][0], redMax);
-        greenMax = Math.max(rgb[rgb.length - 1][1], greenMax);
-        blueMax = Math.max(rgb[rgb.length - 1][2], blueMax);
-
-        redMin = Math.min(rgb[rgb.length - 1][0], redMin);
-        greenMin = Math.min(rgb[rgb.length - 1][1], greenMin);
-        blueMin = Math.min(rgb[rgb.length - 1][2], blueMin);
-    }
-
-    let redRange = redMax - redMin;
-    let greenRange = greenMax - greenMin;
-    let blueRange = blueMax - blueMin;
-    let maxRange = Math.max(redRange, greenRange, blueRange);
-    let maxRangeChannel;
-    if (maxRange === redRange) {
-        maxRangeChannel = 0;
-    } else if (maxRangeChannel === greenRange) {
-        maxRangeChannel = 1;
-    } else {
-        maxRangeChannel = 2;
-    }
-
-    // Part 2: Sort the pixels according with the greatest range channel
-
-    rgb.sort((firstPixel, secondPixel) => firstPixel[maxRangeChannel] - secondPixel[maxRangeChannel]);
-
-    // Part 3: Divide the pixels into buckets, according with the number of dominant colors we want
-
-    // In this specific case, we want 4 colors, i.e., 4 buckets
-
-    let mean = Math.floor(rgb.length / 2);
-    let upperHalf = rgb.slice(0, mean);
-    let lowerHalf = rgb.slice(mean + 1);
-
-    let buckets = [];
-
-    mean = Math.floor(upperHalf.length / 2);
-    buckets.push(upperHalf.slice(0, mean));
-    buckets.push(upperHalf.slice(mean + 1));
-
-    mean = Math.floor(lowerHalf.length / 2);
-    buckets.push(lowerHalf.slice(0, mean));
-    buckets.push(lowerHalf.slice(mean + 1));
-
-    // Part 4: Get the 4 most dominant colors by getting the average of the pixels of each bucket
-
-    let colorSum = [
-        [0, 0, 0],
-        [0, 0, 0],
-        [0, 0, 0],
-        [0, 0, 0]
-    ];
-    for (let i = 0; i < 4; i++) {
-        for (let j = 0; j < buckets[i].length; j++) {
-            colorSum[i][0] += buckets[i][j][0];
-            colorSum[i][1] += buckets[i][j][1];
-            colorSum[i][2] += buckets[i][j][2];
+        let euclideanDistance;
+        let closestEuclideanDistance = Number.MAX_VALUE;
+        let closestCentroid;
+        
+        for (let j = 0; j < centroids.length; j++) {
+            euclideanDistance = Math.sqrt(Math.pow(rgb[0] - centroids[j][0], 2) + Math.pow(rgb[1] - centroids[j][1], 2) + Math.pow(rgb[2] - centroids[j][2], 2));
+            if (euclideanDistance < closestEuclideanDistance) {
+                closestEuclideanDistance = euclideanDistance;
+                closestCentroid = j;
+            }
         }
+
+        clusterLength[closestCentroid]++;
+
+        // Update the centroid
+    
+        sum[closestCentroid][0] += rgb[0];
+        sum[closestCentroid][1] += rgb[1];
+        sum[closestCentroid][2] += rgb[2];
+
+        centroids[closestCentroid][0] = sum[closestCentroid][0] / clusterLength[closestCentroid];
+        centroids[closestCentroid][1] = sum[closestCentroid][1] / clusterLength[closestCentroid];
+        centroids[closestCentroid][2] = sum[closestCentroid][2] / clusterLength[closestCentroid];
+    }
+    
+    for (let i = 0; i < k; i++) {
+        centroids[i][0] = Math.floor(centroids[i][0]);
+        centroids[i][1] = Math.floor(centroids[i][1]);
+        centroids[i][2] = Math.floor(centroids[i][2]);
     }
 
-    let color1 = [Math.floor(colorSum[0][0] / buckets[0].length), Math.floor(colorSum[0][1] / buckets[0].length), Math.floor(colorSum[0][2] / buckets[0].length)];
-    let color2 = [Math.floor(colorSum[1][0] / buckets[1].length), Math.floor(colorSum[1][1] / buckets[1].length), Math.floor(colorSum[1][2] / buckets[1].length)];
-    let color3 = [Math.floor(colorSum[2][0] / buckets[2].length), Math.floor(colorSum[2][1] / buckets[2].length), Math.floor(colorSum[2][2] / buckets[2].length)];
-    let color4 = [Math.floor(colorSum[3][0] / buckets[3].length), Math.floor(colorSum[3][1] / buckets[3].length), Math.floor(colorSum[3][2] / buckets[3].length)];
+    let color1 = centroids[0];
+    let color2 = centroids[1];
+    let color3 = centroids[2];
+    let color4 = centroids[3];
+
+    console.log(color1[0], color1[1], color1[2])
+    console.log(color2[0], color2[1], color2[2])
+    console.log(color3[0], color3[1], color3[2])
+    console.log(color4[0], color4[1], color4[2])
 
     color1Element.innerHTML = `<div class="color-square" style="background-color: rgb(${color1[0]}, ${color1[1]}, ${color1[2]})"></div>${getColorName(getClosestColor(rgbToHex(color1[0], color1[1], color1[2])))}`;
     color2Element.innerHTML = `<div class="color-square" style="background-color: rgb(${color2[0]}, ${color2[1]}, ${color2[2]})"></div>${getColorName(getClosestColor(rgbToHex(color2[0], color2[1], color2[2])))}`;
