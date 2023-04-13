784bff3c0a5a37bd90f965e72b5cdf79bfbd4987
Théo Giovedi <tmgiovedi@gmail.com>
2023-04-13 01:17:10 -0300

    add new Paletas

--- qual-a-cor/scripts/palettes.js
+++ qual-a-cor/scripts/palettes.js
@@ -1,24 +1,29 @@
-import { getClosestColor, rgbToHex } from "./calc.js";
+import { rgbToHex, getClosestColor } from "./calc.js";
 import { getColorName } from "./colors.js";
 
 const cam = document.createElement("video");
+const type = document.getElementById("type").value;
 const camCanvas = document.createElement("canvas");
+camCanvas.width = 0;
+camCanvas.height = 0;
 const camDiv = document.getElementById("cam");
-const color1Element = document.createElement("p");
-const color2Element = document.createElement("p");
-const color3Element = document.createElement("p");
-const color4Element = document.createElement("p");
 camDiv.appendChild(camCanvas);
-camDiv.appendChild(color1Element);
-camDiv.appendChild(color2Element);
-camDiv.appendChild(color3Element);
-camDiv.appendChild(color4Element);
+const paletteButton = document.getElementById("palette-button");
+const color1Element = document.getElementById("color1");
+color1Element.style.display = 'none';
+const color2Element = document.getElementById("color2");
+color2Element.style.display = 'none';
+const color3Element = document.getElementById("color3");
+color3Element.style.display = 'none';
+const color4Element = document.getElementById("color4");
+color4Element.style.display = 'none';
 const errorMessage = document.createElement("p");
 const camContext = camCanvas.getContext("2d", { willReadFrequently: true });
 let hasVideoInput = false;
 let w, h;
 
-let windowSizes = [ window.innerWidth, window.innerHeight ]
+let windowSizes = [window.innerWidth, window.innerHeight]
+let isPaused = false;
 
 function updateCanvas() {
     w = windowSizes[0] - 40;
@@ -33,7 +38,7 @@ function updateCanvas() {
     }
     camCanvas.width = w;
     camCanvas.height = h;
-    windowSizes = [ windowSizes[1], windowSizes[0] ];
+    windowSizes = [windowSizes[1], windowSizes[0]];
 }
 
 function camSetup() {
@@ -44,6 +49,7 @@ function camSetup() {
             }
         })
     }).catch(() => {
+        outerHeight
         errorMessage.innerHTML = "Erro: Não foi possível verificar se seu navegador possui uma câmera";
         camDiv.appendChild(errorMessage);
         return;
@@ -69,20 +75,69 @@ function camSetup() {
 }
 
 function camDraw() {
-
     if (!hasVideoInput) {
         return;
     }
 
-    camContext.drawImage(cam, 0, 0, w, h);
-    getPalette();
+    if (!isPaused) {
+        camContext.drawImage(cam, 0, 0, w, h);
+        let camPixels = camContext.getImageData(0, 0, w, h);
+
+        const type = document.getElementById("type").value;
+        switch (type) {
+            case "normal":
+                break;
+            case "prot":
+                for (let i = 0; i < camPixels.data.length; i += 4) {
+                    const rgb = [
+                        camPixels.data[i] / 255,
+                        camPixels.data[i + 1] / 255,
+                        camPixels.data[i + 2] / 255
+                    ]
+
+                    camPixels.data[i] = ((rgb[0] * 0.152286) + (rgb[1] * 1.052583) + (rgb[2] * -0.204868)) * 255;
+                    camPixels.data[i + 1] = ((rgb[0] * 0.114503) + (rgb[1] * 0.786281) + (rgb[2] * 0.099216)) * 255;
+                    camPixels.data[i + 2] = ((rgb[0] * -0.003882) + (rgb[1] * -0.048116) + (rgb[2] * 1.051998)) * 255;
+                }
+                break;
+            case "deut":
+                for (let i = 0; i < camPixels.data.length; i += 4) {
+                    const rgb = [
+                        camPixels.data[i] / 255,
+                        camPixels.data[i + 1] / 255,
+                        camPixels.data[i + 2] / 255
+                    ]
+
+                    camPixels.data[i] = ((rgb[0] * 0.367322) + (rgb[1] * 0.860646) + (rgb[2] * -0.227968)) * 255;
+                    camPixels.data[i + 1] = ((rgb[0] * 0.280085) + (rgb[1] * 0.672501) + (rgb[2] * 0.047413)) * 255;
+                    camPixels.data[i + 2] = ((rgb[0] * -0.011820) + (rgb[1] * 0.042940) + (rgb[2] * 0.968881)) * 255;
+                }
+                break;
+            case "trit":
+                for (let i = 0; i < camPixels.data.length; i += 4) {
+                    const rgb = [
+                        camPixels.data[i] / 255,
+                        camPixels.data[i + 1] / 255,
+                        camPixels.data[i + 2] / 255
+                    ]
+
+                    camPixels.data[i] = ((rgb[0] * 1.255528) + (rgb[1] * -0.076749) + (rgb[2] * -0.178779)) * 255;
+                    camPixels.data[i + 1] = ((rgb[0] * -0.078411) + (rgb[1] * 0.930809) + (rgb[2] * 0.147602)) * 255;
+                    camPixels.data[i + 2] = ((rgb[0] * 0.004733) + (rgb[1] * 0.691367) + (rgb[2] * 0.303900)) * 255;
+                }
+                break;
+        }
+        camContext.putImageData(camPixels, 0, 0);
+    }
+
     setTimeout(camDraw, 1000 / 24); // 24fps
 }
+
 
 function getPalette() {
     let camPixels = camContext.getImageData(0, 0, w, h);
 
-    // Mean Cut Algorithm to determine the most dominant colors in an image
+    // Median Cut Algorithm to determine the most dominant colors in an image
 
     // Part 1: Determine the color channel (R, G or B) with the largest range
 
@@ -95,6 +150,7 @@ function getPalette() {
             camPixels.data[i + 1],
             camPixels.data[i + 2]
         ]);
+
         redMax = Math.max(rgb[rgb.length - 1][0], redMax);
         greenMax = Math.max(rgb[rgb.length - 1][1], greenMax);
         blueMax = Math.max(rgb[rgb.length - 1][2], blueMax);
@@ -103,14 +159,15 @@ function getPalette() {
         greenMin = Math.min(rgb[rgb.length - 1][1], greenMin);
         blueMin = Math.min(rgb[rgb.length - 1][2], blueMin);
     }
+
     let redRange = redMax - redMin;
     let greenRange = greenMax - greenMin;
     let blueRange = blueMax - blueMin;
-    let maxRange = Math.min(redRange, greenRange, blueRange);
+    let maxRange = Math.max(redRange, greenRange, blueRange);
     let maxRangeChannel;
     if (maxRange === redRange) {
         maxRangeChannel = 0;
-    } else if (maxRangeChannel === blueRange) {
+    } else if (maxRangeChannel === greenRange) {
         maxRangeChannel = 1;
     } else {
         maxRangeChannel = 2;
@@ -163,10 +220,33 @@ function getPalette() {
     color2Element.innerHTML = `<div class="color-square" style="background-color: rgb(${color2[0]}, ${color2[1]}, ${color2[2]})"></div>${getColorName(getClosestColor(rgbToHex(color2[0], color2[1], color2[2])))}`;
     color3Element.innerHTML = `<div class="color-square" style="background-color: rgb(${color3[0]}, ${color3[1]}, ${color3[2]})"></div>${getColorName(getClosestColor(rgbToHex(color3[0], color3[1], color3[2])))}`;
     color4Element.innerHTML = `<div class="color-square" style="background-color: rgb(${color4[0]}, ${color4[1]}, ${color4[2]})"></div>${getColorName(getClosestColor(rgbToHex(color4[0], color4[1], color4[2])))}`;
+
+    paletteButton.innerHTML = "Apagar paleta"
+    color1Element.style.display = "block";
+    color2Element.style.display = "block";
+    color3Element.style.display = "block";
+    color4Element.style.display = "block";
 };
+
+function deletePalette() {
+    paletteButton.innerHTML = "Extrair paleta"
+    color1Element.style.display = "none";
+    color2Element.style.display = "none";
+    color3Element.style.display = "none";
+    color4Element.style.display = "none";
+}
+
+paletteButton.addEventListener("click", () => {
+    if (isPaused) {
+        deletePalette();
+    } else {
+        getPalette();
+    }
+    isPaused = !isPaused;
+    console.log(isPaused);
+})
 
 window.addEventListener("load", camSetup);
 cam.addEventListener("playing", camDraw);
 
-let viewport = window.matchMedia("(orientation: landscape)")
-viewport.addEventListener("change", updateCanvas);
+window.matchMedia("(orientation: landscape)").addEventListener("change", updateCanvas);
--- qual-a-cor/scripts/photos.js
+++ qual-a-cor/scripts/photos.js
@@ -42,133 +42,40 @@ function showPhoto() {
         case "prot":
             for (let i = 0; i < photoPixels.data.length; i += 4) {
                 const rgb = [
-                    [photoPixels.data[i] / 255],
-                    [photoPixels.data[i + 1] / 255],
-                    [photoPixels.data[i + 2] / 255]
+                    photoPixels.data[i] / 255,
+                    photoPixels.data[i + 1] / 255,
+                    photoPixels.data[i + 2] / 255
                 ]
 
-                const matrix = [
-                    [0.152286, 1.052583, -0.204868],
-                    [0.114503, 0.786281, 0.099216],
-                    [-0.003882, -0.048116, 1.051998]
-                ]
-
-                const simulatedRgb = [
-                    [(rgb[0][0] * matrix[0][0]) + (rgb[1][0] * matrix[0][1]) + (rgb[2][0] * matrix[0][2])],
-                    [(rgb[0][0] * matrix[1][0]) + (rgb[1][0] * matrix[1][1]) + (rgb[2][0] * matrix[1][2])],
-                    [(rgb[0][0] * matrix[2][0]) + (rgb[1][0] * matrix[2][1]) + (rgb[2][0] * matrix[2][2])]
-                ]
-
-                if (simulatedRgb[0][0] < 0) {
-                    simulatedRgb[0][0] = 0;
-                } else if (simulatedRgb[0][0] > 1) {
-                    simulatedRgb[0][0] = 1;
-                }
-
-                if (simulatedRgb[1][0] < 0) {
-                    simulatedRgb[1][0] = 0;
-                } else if (simulatedRgb[1][0] > 1) {
-                    simulatedRgb[1][0] = 1;
-                }
-
-
-                if (simulatedRgb[2][0] < 0) {
-                    simulatedRgb[2][0] = 0;
-                } else if (simulatedRgb[2][0] > 1) {
-                    simulatedRgb[2][0] = 1;
-                }
-
-                photoPixels.data[i] = Math.floor(simulatedRgb[0][0] * 255);
-                photoPixels.data[i + 1] = Math.floor(simulatedRgb[1][0] * 255);
-                photoPixels.data[i + 2] = Math.floor(simulatedRgb[2][0] * 255);
+                photoPixels.data[i] = ((rgb[0] * 0.152286) + (rgb[1] * 1.052583) + (rgb[2] * -0.204868)) * 255;
+                photoPixels.data[i + 1] = ((rgb[0] * 0.114503) + (rgb[1] * 0.786281) + (rgb[2] * 0.099216)) * 255;
+                photoPixels.data[i + 2] = ((rgb[0] * -0.003882) + (rgb[1] * -0.048116) + (rgb[2] * 1.051998)) * 255;
             }
             break;
         case "deut":
             for (let i = 0; i < photoPixels.data.length; i += 4) {
                 const rgb = [
-                    [photoPixels.data[i] / 255],
-                    [photoPixels.data[i + 1] / 255],
-                    [photoPixels.data[i + 2] / 255]
+                    photoPixels.data[i] / 255,
+                    photoPixels.data[i + 1] / 255,
+                    photoPixels.data[i + 2] / 255
                 ]
 
-                const matrix = [
-                    [0.367322, 0.860646, -0.227968],
-                    [0.280085, 0.672501, 0.047413],
-                    [-0.011820, 0.042940, 0.968881]
-                ]
-
-                const simulatedRgb = [
-                    [(rgb[0][0] * matrix[0][0]) + (rgb[1][0] * matrix[0][1]) + (rgb[2][0] * matrix[0][2])],
-                    [(rgb[0][0] * matrix[1][0]) + (rgb[1][0] * matrix[1][1]) + (rgb[2][0] * matrix[1][2])],
-                    [(rgb[0][0] * matrix[2][0]) + (rgb[1][0] * matrix[2][1]) + (rgb[2][0] * matrix[2][2])]
-                ]
-
-                if (simulatedRgb[0][0] < 0) {
-                    simulatedRgb[0][0] = 0;
-                } else if (simulatedRgb[0][0] > 1) {
-                    simulatedRgb[0][0] = 1;
-                }
-
-                if (simulatedRgb[1][0] < 0) {
-                    simulatedRgb[1][0] = 0;
-                } else if (simulatedRgb[1][0] > 1) {
-                    simulatedRgb[1][0] = 1;
-                }
-
-
-                if (simulatedRgb[2][0] < 0) {
-                    simulatedRgb[2][0] = 0;
-                } else if (simulatedRgb[2][0] > 1) {
-                    simulatedRgb[2][0] = 1;
-                }
-
-                photoPixels.data[i] = Math.floor(simulatedRgb[0][0] * 255);
-                photoPixels.data[i + 1] = Math.floor(simulatedRgb[1][0] * 255);
-                photoPixels.data[i + 2] = Math.floor(simulatedRgb[2][0] * 255);
+                photoPixels.data[i] = ((rgb[0] * 0.367322) + (rgb[1] * 0.860646) + (rgb[2] * -0.227968)) * 255;
+                photoPixels.data[i + 1] = ((rgb[0] * 0.280085) + (rgb[1] * 0.672501) + (rgb[2] * 0.047413)) * 255;
+                photoPixels.data[i + 2] = ((rgb[0] * -0.011820) + (rgb[1] * 0.042940) + (rgb[2] * 0.968881)) * 255;
             }
             break;
         case "trit":
             for (let i = 0; i < photoPixels.data.length; i += 4) {
                 const rgb = [
-                    [photoPixels.data[i] / 255],
-                    [photoPixels.data[i + 1] / 255],
-                    [photoPixels.data[i + 2] / 255]
+                    photoPixels.data[i] / 255,
+                    photoPixels.data[i + 1] / 255,
+                    photoPixels.data[i + 2] / 255
                 ]
 
-                const matrix = [
-                    [1.255528, -0.076749, -0.178779],
-                    [-0.078411, 0.930809, 0.147602],
-                    [0.004733, 0.691367, 0.303900]
-                ]
-
-                const simulatedRgb = [
-                    [(rgb[0][0] * matrix[0][0]) + (rgb[1][0] * matrix[0][1]) + (rgb[2][0] * matrix[0][2])],
-                    [(rgb[0][0] * matrix[1][0]) + (rgb[1][0] * matrix[1][1]) + (rgb[2][0] * matrix[1][2])],
-                    [(rgb[0][0] * matrix[2][0]) + (rgb[1][0] * matrix[2][1]) + (rgb[2][0] * matrix[2][2])]
-                ]
-
-                if (simulatedRgb[0][0] < 0) {
-                    simulatedRgb[0][0] = 0;
-                } else if (simulatedRgb[0][0] > 1) {
-                    simulatedRgb[0][0] = 1;
-                }
-
-                if (simulatedRgb[1][0] < 0) {
-                    simulatedRgb[1][0] = 0;
-                } else if (simulatedRgb[1][0] > 1) {
-                    simulatedRgb[1][0] = 1;
-                }
-
-
-                if (simulatedRgb[2][0] < 0) {
-                    simulatedRgb[2][0] = 0;
-                } else if (simulatedRgb[2][0] > 1) {
-                    simulatedRgb[2][0] = 1;
-                }
-
-                photoPixels.data[i] = Math.floor(simulatedRgb[0][0] * 255);
-                photoPixels.data[i + 1] = Math.floor(simulatedRgb[1][0] * 255);
-                photoPixels.data[i + 2] = Math.floor(simulatedRgb[2][0] * 255);
+                photoPixels.data[i] = ((rgb[0] * 1.255528) + (rgb[1] * -0.076749) + (rgb[2] * -0.178779)) * 255;
+                photoPixels.data[i + 1] = ((rgb[0] * -0.078411) + (rgb[1] * 0.930809) + (rgb[2] * 0.147602)) * 255;
+                photoPixels.data[i + 2] = ((rgb[0] * 0.004733) + (rgb[1] * 0.691367) + (rgb[2] * 0.303900)) * 255;
             }
             break;
     }
@@ -188,8 +95,7 @@ photo.addEventListener("load", () => {
     showPhoto();
 });
 
-let screenOrientation = window.matchMedia("(orientation: portrait)")
-screenOrientation.addEventListener("change", () => {
+window.matchMedia("(orientation: portrait)").addEventListener("change", () => {
     toggleOrientation();
     updateCanvas();
     showPhoto();
