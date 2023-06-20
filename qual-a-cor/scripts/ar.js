30957ff0dc0e871b064a34884727a1180de7c0e2
Théo Giovedi <tmgiovedi@gmail.com>
2023-06-20 18:11:47 -0300

    fix: correct spacing and distance

--- qual-a-cor/scripts/ar.js
+++ qual-a-cor/scripts/ar.js
@@ -44,7 +44,7 @@ const footer = document.getElementById("footer");
 
 // Global variables used in the functions bellow
 
-let w = (Math.max(window.screen.width, window.screen.height) / 2) + 100,
+let w = (Math.max(window.screen.width, window.screen.height) / 2) + 150,
   h = Math.max(window.screen.width, window.screen.height);
 let clickCounter = 0;
 
